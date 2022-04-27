2742e562f52409a0adf36f876aab061f83e40f66
Casey Harding <casey.r.harding@gmail.com>
2022-04-27 12:46:20 -0400

    Initial GET request in place.

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,23 @@
 // Here goes the schema for the form
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required("Username is required ya jerk!")
+    .min(3, "C'mon, chump, gotta have 3 characters here!"),
+  email: yup
+    .string()
+    .email("Gotta use a valid email address, whattuh you on? aol")
+    .required("Email is required!!"),
+  role: yup
+    .string()
+    .oneOf(["instructor", "student", "alumni"], "Role is required bud!"),
+  civil: yup
+    .string()
+    .oneOf(["married", "single"], "Who do you think you are? You're either married or single kid!"),
+  coding: yup.boolean(),
+  reading: yup.boolean(),
+  hiking: yup.boolean()
+})
